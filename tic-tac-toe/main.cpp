95e4363f018c0e99cb888452fb282cfbae1404ab
aycanurmentes <126423511+aycanurmentes@users.noreply.github.com>
2023-06-03 23:20:28 +0300

    Initial Commit

---
+++ tic-tac-toe/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tic-tac-toe
+//
+//  Created by ayça on 3.06.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
